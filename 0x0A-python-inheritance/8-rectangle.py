b1cfc106432b78ff9cd0a70e215102692db9e2c8
oluzingh <adeyanjuoluzingh@gmail.com>
2022-07-05 04:16:35 +0800

    08

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module 8-rectangle
+Contains parent class BaseGeometry
+with public instance method area and integer_validator
+Contains subclass Rectangle
+with instantiation of private attributes width and height, validated by parent
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry

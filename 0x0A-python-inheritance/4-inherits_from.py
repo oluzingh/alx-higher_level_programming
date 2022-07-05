93b6e5d8c685c0c7f4f61fb7e799b122d69841f1
oluzingh <adeyanjuoluzingh@gmail.com>
2022-07-05 16:29:18 +0800

    04

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,21 +1,9 @@
 #!/usr/bin/python3
-"""
-a function that returns True if the object is exactly
-an instance of the specified class ; otherwise False.
-"""
+"""Defines an inherited class-checking function."""
 
 
 def inherits_from(obj, a_class):
-    """ Return True or False"""
-    return(issubclass(type(obj), a_class) and type(obj) != a_class)
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
+    """Checks if an object is an inherited instance of a class."""
+    if issubclass(type(obj), a_class) and (type(obj) != a_class):
+        return True
+    return False
